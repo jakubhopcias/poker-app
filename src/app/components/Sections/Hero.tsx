eb4334c76fd2924ebbb68dfaabc6b01a20e6d94e
Jakub Hopciaś <hopciasjakub1@gmail.com>
2026-03-23 00:27:55 +0100

    Favicon

--- src/app/components/Sections/Hero.tsx
+++ src/app/components/Sections/Hero.tsx
@@ -1,9 +1,15 @@
 export default function Hero() {
-    return (
-        <section className="max-w-[90%] m-auto p-8 rounded-2xl">
-                <h1>Pokerek</h1>
-                <p>Przed grą w pokera nie zbierajcie pieniędzy, w tabeli widoczne są wypłaty po odjęciu dotychczasowych wpłat.</p>
-                <p >~ by january</p>
-            </section>
-    );
+  return (
+    <section className="max-w-[90%] m-auto p-8 rounded-2xl">
+      <h1>Pokerek</h1>
+      <p>
+        Przed grą w pokera nie zbierajcie pieniędzy, w tabeli widoczne są
+        wypłaty po odjęciu dotychczasowych wpłat.
+      </p>
+      <a href="#players" className="primary-btn">
+        Dodaj graczy
+      </a>
+      <p>~ by january</p>
+    </section>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 import Chips from "./components/Sections/Chips";
 import Hero from "./components/Sections/Hero";
 import Players from "./components/Sections/Players";
 import Settings from "./components/Sections/Settings";
+
+export const metadata: Metadata = {
+  title: "Poker App",
+  icons: "/favicon.ico",
+};
 
 export default function Home() {
   return (
